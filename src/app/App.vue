3d60aff6e513653595dd432e7799814f6842e2da
mansurakhmad <mansurakhmad.051294@gmail.com>
2025-12-31 18:15:37 +0300

    init: added base style settings

---
+++ src/app/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="body">
+    <h1>You did it!</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.body {
+  background-color: var(--wine-100);
+}
+</style>
