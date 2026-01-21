f0d0df059ed37a9b242cd828ec54261617296f5b
mansurakhmad <mansurakhmad.051294@gmail.com>
2026-01-21 22:59:50 +0300

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import App from '@/app/App.vue';
 import PrimeVue from 'primevue/config';
 import { router } from '@/app/router';
 import Aura from '@primeuix/themes/aura';
+import { createPinia } from 'pinia';
 import '@/app/styles/index.css';
+
+const pinia = createPinia();
 
 const app = createApp(App);
 
 app.use(PrimeVue, { theme: { preset: Aura } });
+app.use(pinia);
 app.use(router);
 app.mount('#app');
